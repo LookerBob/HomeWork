de8adecfc9012895e61c1227e4782b1cb714108d
LookerBob <okbpower@gmail.com>
2024-08-07 23:15:53 +0500

    не строки а числа

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,7 @@
-first = input()
-second = input()
-third = input()
+print('Введите 3 числа:')
+first = int(input())
+second = int(input())
+third = int(input())
 if first == second == third:
     print('3')
 elif first == second or first == third or second == third:
