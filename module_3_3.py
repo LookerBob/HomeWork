2a84040cf1ba8b57af47761694dcd36e7759ff1b
LookerBob <okbpower@gmail.com>
2024-08-19 20:41:40 +0500

    initial

---
+++ module_3_3.py
@@ -0,0 +1,17 @@
+def print_params(a=1, b='строка', c=True):
+  print(a, b, c)
+
+print_params()
+print_params(2)
+print_params(1, 2, 4)
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = [500, 'мискузи', False]
+values_dict = {'a': 250, 'b': 'массаракш', 'c': False}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [2, 'туземун']
+print_params(*values_list_2, 42)
